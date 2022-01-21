10fff614027a22bb74694e8c4c07d4c904a7254b
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2022-01-21 20:20:58 +0900

    fix storybook next image error issue

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useRef } from 'react';
 import styled, { css } from 'styled-components';
 import Head from 'next/head';
 
+// import RegisterComponent from 'UIpages/auth/register/index';
 import { RegisterComponent } from 'UIpages/index';
 
 const RegisterPage: React.FC<any> = ({}) => {
