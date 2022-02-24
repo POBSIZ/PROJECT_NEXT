6c41e08c8eeac6abc0c902674480be1a8c6111bf
iamiks <rmstjr1030@naver.com>
2022-02-24 17:47:01 +0900

    Feat: apply addon-knobs addon-viewport

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   stories: [
     '../src/**/*.stories.(js|jsx|ts|tsx|mdx)',
     '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+    '@storybook/addon-viewport',
+  ],
   framework: '@storybook/react',
   webpackFinal: async (config) => {
-    
-    config.resolve.alias.Bases = __dirname + "/../src/components/base/";
-    config.resolve.alias.Actions = __dirname + "/../src/redux/actions/";
-    config.resolve.alias.Templates = __dirname + "/../src/components/templates/";
-    config.resolve.alias.UIpages = __dirname + "/../src/components/pages/";
-    config.resolve.alias.Atoms = __dirname + "/../src/components/UI/atoms/";
-    config.resolve.alias.Molecules = __dirname + "/../src/components/UI/molecules/";
-    config.resolve.alias.Organisms = __dirname + "/../src/components/UI/organisms/";
-    config.resolve.alias.Hoc = __dirname + "/../src/components/HOC/";
-    config.resolve.alias.Utils = __dirname + "/../src/utils/";
-    config.resolve.alias.StyleVars = __dirname + "/../styles/global_styles.tsx";
-    config.resolve.alias.StoriesLayout = __dirname + "/../src/components/UI/stories/storiesLayout/index.tsx";
-    
+    config.resolve.alias.Bases = __dirname + '/../src/components/base/';
+    config.resolve.alias.Actions = __dirname + '/../src/redux/actions/';
+    config.resolve.alias.Templates =
+      __dirname + '/../src/components/templates/';
+    config.resolve.alias.UIpages = __dirname + '/../src/components/pages/';
+    config.resolve.alias.Atoms = __dirname + '/../src/components/UI/atoms/';
+    config.resolve.alias.Molecules =
+      __dirname + '/../src/components/UI/molecules/';
+    config.resolve.alias.Organisms =
+      __dirname + '/../src/components/UI/organisms/';
+    config.resolve.alias.Hoc = __dirname + '/../src/components/HOC/';
+    config.resolve.alias.Utils = __dirname + '/../src/utils/';
+    config.resolve.alias.StyleVars = __dirname + '/../styles/global_styles.tsx';
+    config.resolve.alias.StoriesLayout =
+      __dirname + '/../src/components/UI/stories/storiesLayout/index.tsx';
+
     config.resolve.extensions.push(
       '.ts',
       '.tsx',
